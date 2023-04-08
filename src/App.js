f112fc13237fab56a68255835908128eeabe6f1e
HarshaMacherla <macherlaharsha997@gmail.com>
2023-04-08 21:45:44 +0530

    Added Filter by year functionality

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2023, 3, 4),
+      date: new Date(2020, 3, 4),
     },
     {
       id: "e2",
       title: "New TV",
       amount: 799.49,
-      date: new Date(2023, 3, 4),
+      date: new Date(2021, 3, 4),
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2023, 3, 4),
+      date: new Date(2022, 3, 4),
     },
     {
       id: "e4",
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,28 +2,16 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
-
-  const clickHandler = () => {
-    setTitle("Updated!!!");
-    console.log(title);
-  };
-
-  const amountHandler = () => {
-    setAmount(100.0);
-    console.log(amount);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails amount={amount} location={props.location} title={title} />
-      <button onClick={clickHandler}>Change Title</button>
-      <button onClick={amountHandler}>Change Expense</button>
+      <ExpenseDetails
+        amount={props.amount}
+        location={props.location}
+        title={props.title}
+      />
     </Card>
   );
 };
